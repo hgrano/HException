a3e793cf9ef65d82647fa2d969e5ff66e24c6c2c
Huw Grano <huw.grano@gmail.com>
2020-05-27 19:07:11 +1000

    re-write main code to use ExceptT

--- src/HError.hs
+++ src/HError.hs
@@ -19,11 +19,13 @@ module HError(
   Error1,
   Result,
   Result1,
+  ResultT,
+  ResultT1,
   Value,
   TypeIndexed,
   Handler,
   Done,
-  -- * Pure functions for working with @Result@s
+  -- * General functions for working with @Result@s
   Generalizable,
   generalize,
   extend,
@@ -46,6 +48,8 @@ module HError(
 ) where
 
 import qualified Control.Exception     as E
+import qualified Control.Monad.Trans.Except as TE
+import           Data.Functor.Identity (Identity)
 import qualified Data.HList.CommonMain as H
 import qualified Data.HList.TIC        as T
 import qualified Data.HList.TIP        as TP
@@ -79,36 +83,43 @@ type Only e = e :^: '[]
 -- | An error of only one type.
 type Error1 e = Error (Only e)
 
--- | Type for returning results from computations which may fail with one of a known set of errors @es@ or return a
+-- | Type for returning results from pure computations which may fail with one of a known set of errors @es@ or return a
 -- value @a@.
-type Result es a = Either (Error es) a
+type Result es = ResultT es Identity
 
 -- | A specialization of 'Result' for computations which can return only one type of error.
-type Result1 e a = Either (Error1 e) a
+type Result1 e = Result (Only e)
+
+-- | A generalization of 'Result' for computations lifted into a monad @m@.
+type ResultT es m = TE.ExceptT (Error es) m
+
+-- | A specialization of 'ResultT' for computations which can return only one type of error.
+type ResultT1 e m = ResultT (Only e) m
 
 -- | A specialization of 'Result' for computations in which all possible error types have been dealt with using
 -- 'recover' or 'recovers'. This means the domain of possible errors is empty.
-type Value a = Result '[] a
+type Value m = ValueT Identity
+
+-- | A generalization of 'Value' for computations lifted into a monad @m@.
+type ValueT m = ResultT '[] m
 
 -- | Constrain that the type-list @xs@ has no duplicate types, and so can be indexed via type only.
 type TypeIndexed xs = (TP.HAllTaggedEq xs, H.HLabelSet (H.LabelsOf xs), H.HAllTaggedLV xs)
 
--- | Constrain that the input type list @xs@ contains all of the elements of the output type-list @xs'@ and that @xs'@
--- is 'TypeIndexed'.
-type Generalizable xs xs' = (TypeIndexed xs', V.ExtendsVariant xs xs')
+-- | Constrain that the input type list @xs@ contains all of the elements of the output type-list @xs'@.
+type Generalizable xs xs' = V.ExtendsVariant xs xs'
 
 -- | Changes the type of an 'Error' so it is more general than the original. The underlying stored value
 -- is left unchanged. For most use cases 'extend' will likely be more suitable than 'generalize'. Note: this can also
 -- be used to re-order the error types.
-generalize :: Generalizable es es' => Error es -> Error es'
+generalize :: (Generalizable es es', TypeIndexed es') => Error es -> Error es'
 generalize (Error (T.TIC v)) = Error . T.TIC $ H.extendsVariant v
 
 -- | Extend the given result so that it may be used in a context which can return a superset of the errors that may
 -- arise from the original result. This is useful for calling multiple functions which return different errors types
 -- from within a single function. Note: this can also be used to re-order the error types.
-extend :: Generalizable es es' => Result es a -> Result es' a
-extend (Left e)  = Left $ generalize e
-extend (Right x) = Right x
+extend :: (Generalizable es es', Monad m, TypeIndexed es') => ResultT es m a -> ResultT es' m a
+extend r = TE.catchE r (TE.throwE .  generalize)
 
 -- Constraint such that the type @x@ may exist in the co-product of types @xs@.
 type Gettable xs x = (H.HasField x (H.TIC xs) (Maybe x))
@@ -125,29 +136,29 @@ get (Error (T.TIC v)) = H.unvariant v
 -- | The type @x@ exists in @xs@ at index @n@.
 type CoProductMember x xs n = (H.HasField x (H.Record xs) x,
                                H.HFind1 x (H.UnLabel x (H.LabelsOf xs)) (H.UnLabel x (H.LabelsOf xs)) n,
-                               KnownNat (H.HNat2Nat n),
-                               TypeIndexed xs)
+                               KnownNat (H.HNat2Nat n))
 
 -- | Lift a standard exception value into an 'Error'.
-err :: CoProductMember e es n => e -> Error es
+err :: (CoProductMember e es n, TypeIndexed es) => e -> Error es
 err = Error . H.mkTIC
 
 -- | Return an error result using the provided error. HError equivalent of "throwing" an exception - but a value is
 -- returned instead of being thrown.
-raise :: CoProductMember e es n => e -> Result es a
-raise = Left . err
+raise :: (CoProductMember e es n, Monad m, TypeIndexed es) => e -> ResultT es m a
+raise = TE.throwE . err
 
 -- | The type of functions used to handle errors.
-type Handler es es' a = Error es -> Result es' a
+type HandlerT es es' m a = Error es -> ResultT es' m a
+
+type Handler es es' a = HandlerT es es' Identity a
 
 -- | Handle errors and return a new result. HError equivalent of "catching" an exception.
-recover :: Result es a -> Handler es es' a -> Result es' a
-recover (Left e) f  = f e
-recover (Right x) _ = Right x
+recover :: Monad m => ResultT es m a -> HandlerT es es' m a -> ResultT es' m a
+recover = TE.catchE
 
-class Recovers es fs es' a | es fs -> es' a where
+class Monad m => Recovers es fs es' m a | es fs -> es' m a where
   -- | Handle errors using a heterogeneous list of 'Handler's, and (optionally) a default value.
-  recovers :: Result es a -> H.HList fs -> Result es' a
+  recovers :: ResultT es m a -> H.HList fs -> ResultT es' m a
   infixr 1 `recovers`
 
 -- | Delete all members of the type-list @l@ from the type-list @m@.
@@ -162,32 +173,30 @@ sliceVariant = H.splitVariant
 
 instance (H.SplitVariant es es' os,
           DeleteAll es' es os,
-          Recovers os (Handler x x' a ': fs) es'' a,
-          TypeIndexed es'') => Recovers es (Handler es' es'' a ': Handler x x' a ': fs) es'' a where
-  recovers (Left (Error (T.TIC v))) fs = case sliceVariant v of
+          Recovers os (HandlerT x x' m a ': fs) es'' m a,
+          TypeIndexed es'') => Recovers es (HandlerT es' es'' m a ': HandlerT x x' m a ': fs) es'' m a where
+  recovers r fs = TE.catchE r $ \(Error (T.TIC v)) -> case sliceVariant v of
     Left e  -> H.hHead fs . Error $ T.TIC e
-    Right o -> recovers (Left (Error (T.TIC o))) $ H.hTail fs
-  recovers (Right x) _ = Right x
+    Right o -> recovers (TE.throwE . Error $ T.TIC o) $ H.hTail fs
 
-instance (H.SameLength es es', H.ExtendsVariant es es', TypeIndexed es'') =>
-         Recovers es '[Handler es' es'' a] es'' a where
-  recovers (Left (Error (T.TIC v))) fs = H.hHead fs . Error . T.TIC $ V.rearrangeVariant v
-  recovers (Right x) _ = Right x
+instance (H.SameLength es es', H.ExtendsVariant es es', Monad m, TypeIndexed es'') =>
+         Recovers es '[HandlerT es' es'' m a] es'' m a where
+  recovers r fs = TE.catchE r $ \(Error (T.TIC v)) ->
+    H.hHead fs . Error . T.TIC $ V.rearrangeVariant v
 
 instance (DeleteAll es' es os,  -- Constrain this so that the instance only applies if not all errors have been handled
          H.HLengthGe os ('H.HSucc 'H.HZero), -- using this length constraint.
          H.ProjectVariant es es',
+         Monad m,
          TypeIndexed es'') =>
-         Recovers es '[Handler es' es'' a, Result es'' a] es'' a where
-  recovers (Left (Error (T.TIC v))) fs = case H.projectVariant v of
+         Recovers es '[HandlerT es' es'' m a, ResultT es'' m a] es'' m a where
+  recovers r fs = TE.catchE r $ \(Error (T.TIC v)) -> case H.projectVariant v of
     Just e  -> H.hHead fs . Error $ T.TIC e
     Nothing -> H.hLast fs
-  recovers (Right x) _ = Right x
 
--- | Chain 'Handler's together.
-orElse :: Handler es es' a -> H.HList fs -> H.HList (Handler es es' a ': fs)
+-- | Chain 'HandlerT's together.
+orElse :: HandlerT es es' m a -> H.HList fs -> H.HList (HandlerT es es' m a ': fs)
 orElse = H.HCons
-
 infixr 2 `orElse`
 
 -- | Type indicating all error types have been handled.
@@ -199,14 +208,17 @@ done = H.HNil
 
 -- | An alternative to @`orElse` done@ which attempts to use the provided 'Handler', or if the 'Handler' does not apply,
 -- returns a default 'Result'.
-orDefault :: Handler es es' a -> Result es' a -> H.HList '[Handler es es' a, Result es' a]
+orDefault :: HandlerT es es' m a -> ResultT es' m a -> H.HList '[HandlerT es es' m a, ResultT es' m a]
 orDefault h r = h `H.HCons` r `H.HCons` H.HNil
 
 -- | Extract the value from a computation result after all errors have been handled. This is type-safe because the
 -- type of errors is constrained to be empty.
-value :: Value a -> a
-value (Right x) = x
-value (Left _) = error "HError internal error: unexpected error inside a 'Value'"
+value :: Monad m => ValueT m a -> m a
+value v = do
+  x <- TE.runExceptT v
+  case x of
+    Right r -> return r
+    Left _ -> error "HError internal error: unexpected error inside a 'Value'"
 
 class Panic es where
   -- | Uses 'E.throw' to throw the 'E.Exception' contained in the 'Error'. Where possible, it is advised to use pure
@@ -219,26 +231,24 @@ instance (E.Exception e, Panic (e' :^: es)) => Panic (e :^: e' :^: es) where
     Left x   -> E.throwIO x
     Right es -> panic . Error $ T.TIC es
 
-instance E.Exception e => Panic '[H.Tagged e e] where
+instance E.Exception e => Panic (Only e) where
   panic = E.throwIO . get
 
 class Attempt es where
-  handleSome :: proxy es -> E.SomeException -> IO (Result es a)
+  handleSome :: proxy es -> E.SomeException -> ResultT es IO a
 
   -- | Attempt the given IO action: if one of the 'E.Exception's in @es@ is thrown then it will be returned on the
   -- @Left@ of the 'Result', if any other 'E.Exception' is thrown it will not be handled, otherwise the @Right@ is
   -- returned.
-  attempt :: IO a -> IO (Result es a)
-  attempt action = (Right <$> action) `E.catch` handleSome (Proxy :: Proxy es)
+  attempt :: IO a -> ResultT es IO a
+  attempt action = TE.ExceptT $ (Right <$> action) `E.catch` (TE.runExceptT . handleSome (Proxy :: Proxy es))
 
 instance (Attempt es, E.Exception e, TypeIndexed (e :^: es)) => Attempt (e :^: es) where
   handleSome _ s = case E.fromException s of
-    Just (x :: e) -> return $ raise x
-    Nothing -> do
-      r <- handleSome (Proxy :: Proxy es) s
-      return $ case r of
-        Left (Error (T.TIC v)) -> Left . Error . T.TIC $ H.extendVariant v
-        Right x                -> Right x
+    Just (x :: e) -> raise x
+    Nothing -> TE.withExceptT
+      (\(Error (T.TIC v)) -> Error . T.TIC $ H.extendVariant v)
+      (handleSome (Proxy :: Proxy es) s)
 
 instance Attempt '[] where
-  handleSome _ = E.throwIO
+  handleSome _ = TE.ExceptT . fmap Right . E.throwIO
